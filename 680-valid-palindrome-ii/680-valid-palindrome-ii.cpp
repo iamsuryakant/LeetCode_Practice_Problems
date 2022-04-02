b4d4a84e35259c4c8c2b9456c55aa9d11d38bcc7
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-04-02 18:16:27 +0530

    Time: 95 ms (14.44%), Space: 23.9 MB (46.56%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    
+public:
+    bool isPali(string s, int st, int end)
+    {
+        // int st = 0, end = s.length();
+        
+        while(st < end)
+        {
+            if(s[st] != s[end]){
+                return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+    }
+    
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r = s.length()-1;
+        
+        while(l < r-1){
+            if(s[l] != s[r])
+            {
+                if(isPali(s, l, r-1))
+                {
+                    return true;
+                }
+                
+                if(isPali(s, l+1, r)){
+                    return true;
+                }
+                
+                return false;
+            }
+            else{
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+};
