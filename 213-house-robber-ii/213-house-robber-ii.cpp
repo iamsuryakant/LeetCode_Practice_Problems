0aa1ec29bd11093e1e7b9d4c4118f2ff14acaa24
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-06-19 22:59:37 +0530

    Time: 0 ms (100.00%), Space: 8 MB (17.19%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -2,21 +2,32 @@ class Solution {
 public:
     int findMaxSum(vector<int> nums) {
     // code here
-        int n = nums.size();
-        int prev2 = 0;
-        int prev1 = nums[0];
+//         int n = nums.size();
+//         int prev2 = 0;
+//         int prev1 = nums[0];
         
-        for(int i = 1; i<n; i++)
-        {
-            int inc = prev2 + nums[i];
-            int ex = prev1;
+//         for(int i = 1; i<n; i++)
+//         {
+//             int inc = prev2 + nums[i];
+//             int ex = prev1;
             
-            int ans = max(inc, ex);
-            prev2 = prev1;
-            prev1 = ans;
-        }
+//             int ans = max(inc, ex);
+//             prev2 = prev1;
+//             prev1 = ans;
+//         }
         
-        return prev1;
+//         return prev1;
+        int n = nums.size();
+        
+        int dp[n+1];
+        
+        dp[0]=0;
+        dp[1]=nums[0];
+        
+        for(int i=2;i<=n;i++)
+            dp[i]=max(dp[i-2]+nums[i-1],dp[i-1]);
+
+        return dp[n];
     }
     
     int rob(vector<int>& nums) {
