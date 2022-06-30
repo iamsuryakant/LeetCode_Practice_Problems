bffbfce3ba1039556b63280955b56b379cc64716
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-06-30 09:47:13 +0530

    Time: 283 ms (5.15%), Space: 28.3 MB (41.83%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int n = nums.size();
+
+	int incre_oper = 0, decre_oper = 0;
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << nums[i] << " ";
+	}
+
+	cout << endl;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		// if (nums[i] != nums[i - 1])
+		// {
+		// 	incre_oper += nums[i - 1] - nums[1];
+		// }
+
+		incre_oper += (nums[i] - nums[0]);
+
+	}
+    
+        return incre_oper;
+    }
+};
