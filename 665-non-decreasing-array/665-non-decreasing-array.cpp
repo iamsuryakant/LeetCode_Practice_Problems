317ec39093e93043d159e0b9bd7e8ccf1773b6cb
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-07-01 13:03:41 +0530

    Time: 58 ms (17.41%), Space: 27.1 MB (53.05%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -11,8 +11,8 @@ public:
         for(int i = 1, count = 0;i<n; i++)
         {
             if(nums[i] < nums[i-1]){
-               // count++;
-                if(count++ || (i > 1 && i<n-1) &&
+                count++;
+                if(count > 1 || (i > 1 && i<n-1) &&
                    (nums[i-2] > nums[i]) && (nums[i+1] < nums[i-1]) ){
                     return false;
                 }
