7e41fc2c28cb06c4c87a28bcf4ab2b240514acf2
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-07-02 23:07:43 +0530

    Time: 3 ms (56.54%), Space: 6.6 MB (51.98%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int n = s.length()-1;
+        // cout<<n<<endl;
+        // return 5;
+        
+        int len = 0;
+       // int j = 0;
+        
+        while( n >= 0 && s[n] == ' '){
+            n--;
+        }
+        
+        while(n >= 0 && s[n] != ' '){
+            len++;
+            n--;
+        }
+        
+        return len;
+    }
+};
