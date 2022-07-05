0395071d5e6f88aa162a09b8dd83726f4ac5156a
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-07-05 14:15:29 +0530

    Time: 3 ms (83.40%), Space: 7.9 MB (42.90%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -31,8 +31,46 @@ public:
         
         //cout<<m<<" "<<n<<endl;
         
-        vector<vector<int>>dp(m, vector<int>(n, -1));
+        vector<vector<int>>dp(m, vector<int>(n, 0));
+         
         
-        return path(m-1, n-1, obstacleGrid, dp);
+        for(int i = 0; i<m; i++)
+        {
+            for(int j = 0; j< n; j++)
+            {
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                   // continue;
+                }else{
+                    int up = 0, left = 0;
+
+                    if(i > 0)
+                    {
+                        up = dp[i-1][j];
+                    }
+
+                    if(j > 0){
+                        left = dp[i][j-1];
+                    }
+
+                    dp[i][j] = up + left;
+                
+                }
+                
+                
+                
+                
+                }
+            }
+        
+        
+        
+            return dp[m-1][n-1];
+        
+        
+       // return path(m-1, n-1, obstacleGrid, dp);
     }
 };
