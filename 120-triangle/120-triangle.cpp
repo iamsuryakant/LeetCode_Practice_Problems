cf2a042559ee551e1a35887db742177afb94d5e1
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-07-06 16:42:21 +0530

    Time: 14 ms (23.30%), Space: 9.1 MB (22.12%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -20,8 +20,29 @@ public:
         
         int n = triangle.size();
         
-        vector<vector<int>>dp(n, vector<int>(n, -1));
+        vector<vector<int>>dp(n, vector<int>(n, 0));
         
-        return solve(0, 0, n, triangle, dp);
+        for(int j = 0;j<n; j++)
+        {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i = n-2; i>=0; i--)
+        {
+           
+            for(int j = i; j>=0; j--)
+            {
+               int down = triangle[i][j] + dp[i+1][j];
+               int dia = triangle[i][j] + dp[i+1][j+1];
+               dp[i][j] = min(down, dia);
+               
+            }
+           
+        }
+        
+        return dp[0][0];
+        
+        
+       // return solve(0, 0, n, triangle, dp);
     }
 };
