165aba40cc3c8166000afed6c8b9ab1e573589c6
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-07-23 21:36:49 +0530

    Time: 1062 ms (33.33%), Space: 177.2 MB (33.33%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cpp
@@ -0,0 +1,16 @@
+class NumberContainers {
+public:
+    unordered_map<int, int> ind_num;
+    unordered_map<int, set<int>> num_inds;
+    void change(int index, int number) {
+        auto it = ind_num.find(index);
+        if (it != end(ind_num))
+            num_inds[it->second].erase(index);
+        ind_num[index] = number;
+        num_inds[number].insert(index);
+    }
+    int find(int number) {
+        auto it = num_inds.find(number);
+        return it == end(num_inds) || it->second.empty() ? -1 : *begin(it->second);
+    }
+};
