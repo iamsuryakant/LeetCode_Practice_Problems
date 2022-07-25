5a0b05b497bd9ff651bda524d7d540bef6e3ef60
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-07-25 08:15:21 +0530

    Time: 12 ms (60.23%), Space: 13.5 MB (98.09%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,27 +1,71 @@
 class Solution {
+    
+private:
+    
+    int firstOcc(vector<int>&arr, int target)
+    {
+        int l = 0, h = arr.size()-1;
+       
+        int ans = -1;
+        
+        while(l<=h)
+        {
+            int mid = l+(h-l)/2;
+            
+            if(arr[mid] == target)
+            {
+                ans = mid;
+                h = mid - 1;
+            }
+            else if(arr[mid] > target)
+            {
+                h = mid-1;
+            }
+            else if(arr[mid] < target)
+            {
+                l = mid+1;
+            }
+        }
+        return ans;
+    }
+    
+    
+    int lastOcc(vector<int>&arr, int target)
+    {
+        int l = 0, h = arr.size()-1;
+        
+        int ans = -1;
+        
+        while(l<=h)
+        {
+            int mid = l+(h-l)/2;
+            
+            if(arr[mid] == target)
+            {
+                ans = mid;
+                l = mid + 1;
+            }
+            else if(arr[mid] > target)
+            {
+                h = mid-1;
+            }
+            else if(arr[mid] < target)
+            {
+                l = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         
-        int n = nums.size();
-        int first = -1, last = -1;
+        vector<int> ans;
         
-        for(int i = 0; i<n; i++)
-        {
-            if(nums[i] == target){
-                first = i;
-                break;
-            }
-        }
+        ans.push_back(firstOcc(nums, target));
+        ans.push_back(lastOcc(nums, target));
         
-        for(int i = n-1; i>=0; i--)
-        {
-            if(nums[i] == target){
-                last = i;
-                break;
-            }
-        }
-        
-        
-        return {first, last};
+        return ans;
     }
 };
