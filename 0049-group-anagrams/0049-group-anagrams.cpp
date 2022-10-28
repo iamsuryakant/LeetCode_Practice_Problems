a2c6dff9cf2181b915b0baf4dc22fe0ae735439c
Suryakant Thakur <suryakantthakur825@gmail.com>
2022-10-28 13:57:57 +0530

    Time: 76 ms (51.25%), Space: 20.8 MB (41.59%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        vector<vector<string>> ans;
+        
+        unordered_map<string, vector<string>> mp;
+        
+        for(string st : strs)
+        {
+           string s = st;
+            
+           sort(s.begin(), s.end());
+            
+            mp[s].push_back(st);
+            
+        }
+        
+       // cout<<mp<<endl;
+        
+        for(auto x: mp){
+            ans.push_back(x.second);
+        }
+        
+        return ans;
+        
+    }
+};
